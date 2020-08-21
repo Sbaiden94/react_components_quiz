e64358caa5775fb4dd9c9e07f80d8e87ed44ca55
Sbaiden94 <50967628+Sbaiden94@users.noreply.github.com>
2020-08-21 21:40:00 +0000

    Add files via upload

---
+++ src/commentDetail.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class CommentDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={this.props.img} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {this.props.author}
+          </a>
+          <div className="metadata">
+            <span className="date">{this.props.timeAgo}</span>
+          </div>
+          <div className="text">{this.props.commentText}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CommentDetail;
