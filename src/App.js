d6d503f017f8bf7e997d84f61cc40b64ceab5cde
Sbaiden94 <50967628+Sbaiden94@users.noreply.github.com>
2020-08-21 21:41:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,57 +8,55 @@ import CommentDetail from "./commentDetail";
 
 
 class App extends Component {
-  componentBuilder() {
-    let data = [
-      {
-        author: "Dennis Obel",
-        timeAgo: "Today at 4:45PM",
-        commentText: "Good job",
-        img: faker.image.avatar(),
-      },
-      {
-        author: "Paul Solomon",
-        timeAgo: "Today at 5:45PM",
-        commentText: "Nice work",
-        img: faker.image.avatar(),
-      },
-      {
-        author: "Peter Nortey",
-        timeAgo: "Today at 6:45PM",
-        commentText: "Great stuff",
-        img: faker.image.avatar(),
-      },
-      {
-        author: "James Asafuah",
-        timeAgo: "Today at 7:45PM",
-        commentText: "Commendable",
-        img: faker.image.avatar(),
-      },
-      {
-        author: "Persius Osei-Tutu",
-        timeAgo: "Today at 7:45PM",
-        commentText: "Commendable",
-        img: faker.image.avatar(),
-      },
-    ];
+  data = [
+    {
+      author: "Dennis Obel",
+      timeAgo: "Today at 4:45PM",
+      commentText: "Good job",
+      img: faker.image.avatar(),
+    },
+    {
+      author: "Paul Solomon",
+      timeAgo: "Today at 5:45PM",
+      commentText: "Nice work",
+      img: faker.image.avatar(),
+    },
+    {
+      author: "Peter Nortey",
+      timeAgo: "Today at 6:45PM",
+      commentText: "Great stuff",
+      img: faker.image.avatar(),
+    },
+    {
+      author: "James Asafuah",
+      timeAgo: "Today at 7:45PM",
+      commentText: "Commendable",
+      img: faker.image.avatar(),
+    },
+    {
+      author: "Persius Osei-Tutu",
+      timeAgo: "Today at 7:45PM",
+      commentText: "Commendable",
+      img: faker.image.avatar(),
+    },
+  ];
 
-    return data
-      .filter((element) => {
-        return element.author.includes("P");
-      })
-      .map((element) => {
-        return (
-          <ApprovalCard>
-            <CommentDetail
-              author={element.author}
-              timeAgo={element.timeAgo}
-              commentText={element.commentText}
-              img={element.img}
-            />
-          </ApprovalCard>
-        );
-      });
-  }
+  componentBuilder = this.data
+    .filter((element) => {
+      return element.author.includes("P");
+    })
+    .map((element) => {
+      return (
+        <ApprovalCard>
+          <CommentDetail
+            author={element.author}
+            timeAgo={element.timeAgo}
+            commentText={element.commentText}
+            img={element.img}
+          />
+        </ApprovalCard>
+      );
+    });
 
   render() {
     return (
@@ -66,7 +64,7 @@ class App extends Component {
         <ApprovalCard>
           <p>Are you sure?</p>
         </ApprovalCard>
-        {this.componentBuilder()}
+        {this.componentBuilder}
       </div>
     );
   }
